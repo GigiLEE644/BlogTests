2735dc366957d937520e7ce4cb6f1cf0e97561d9
lenovo_ma <lenovo_ma@example.com>
2021-12-18 18:19:50 +0100

    modify deadlock

--- java.troubleshooting.standard.tools/DeadLockTest.java
+++ java.troubleshooting.standard.tools/DeadLockTest.java
@@ -1,28 +1,36 @@
-import java.util.concurrent.TimeUnit;
-
 public class DeadLockTest {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Object lock1 = new Object();
         Object lock2 = new Object();
 
-        synchronized (lock2) {
-            TimeUnit.SECONDS.sleep(1);
-            synchronized (lock1) {
-                System.out.println("never enter here");
-            }
-        }
-
-        Thread t = new Thread() {
+        Thread t1 = new Thread() {
             @Override
             public void run() {
-                synchronized (lock1) {
-                    synchronized (lock2) {
-                        System.out.println("never enter here");
+                synchronized (lock2) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (lock1) {
+
                     }
                 }
             }
         };
 
-        t.start();
+        Thread t2 = new Thread() {
+            @Override
+            public void run() {
+                synchronized (lock1) {
+                    synchronized (lock2) {
+
+                    }
+                }
+            }
+        };
+
+        t1.start();
+        t2.start();
     }
 }
