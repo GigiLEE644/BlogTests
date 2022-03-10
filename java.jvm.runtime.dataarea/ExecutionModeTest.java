e518d895a572266a886a3096b6c4d6c8034c8867
yan <yan@li.com>
2022-03-10 13:10:58 +0100

    add execution mode test

---
+++ java.jvm.runtime.dataarea/ExecutionModeTest.java
@@ -0,0 +1,31 @@
+import java.time.Duration;
+import java.time.Instant;
+
+public class ExecutionModeTest {
+    /**
+     * -Xint -Xcomp -Xmixed
+     */
+    public static void main(String[] args) {
+        Instant inst1 = Instant.now();
+        heavyCalculation(1000000);
+        Instant inst2 = Instant.now();
+        System.out.println("Elapsed Time: " + Duration.between(inst1, inst2).toMillis() + " ms");
+    }
+
+    private static void heavyCalculation(int count) {
+        for (int i = 0; i < count; i++) {
+            prime(100);
+        }
+    }
+
+    private static void prime(int n) {
+        label: for (int j = 2; j <= n; j++) {
+            for (int k = 2; k <= Math.sqrt(j); k++) {
+                if (j % k == 0) {
+                    continue label;
+                }
+            }
+            // System.out.println(j);
+        }
+    }
+}
