11e3095e465c952c413ce9fa3bd40fc06c3081d0
yan <yan@li.com>
2022-05-28 21:30:15 +0200

    add print writer test

--- java.io/PrintWriterTest.java
+++ java.io/PrintWriterTest.java
@@ -1,11 +1,12 @@
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintWriter;
 
 public class PrintWriterTest {
     public static void main(String[] args) throws FileNotFoundException {
         String path = "/home/yan/github/BlogTests/greeting";
 
-        try (PrintWriter pw = new PrintWriter(path)) {
+        try (PrintWriter pw = new PrintWriter(new FileOutputStream(path))) {
             pw.println("hello world");
         }
     }
