b03250601408acc5b5082a8e5abfeb0f3b492d47
yan <yan@li.com>
2022-05-29 10:06:01 +0200

    add data input stream test

--- java.io/DataInputStreamTest.java
+++ java.io/DataInputStreamTest.java
@@ -3,16 +3,28 @@ import java.io.DataInputStream;
 import java.io.IOException;
 
 public class DataInputStreamTest {
+    private static class Person {
+        private String name;
+        private int age;
+        private boolean male;
+
+        public Person(String name, int age, boolean male) {
+            this.name = name;
+            this.age = age;
+            this.male = male;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [age=" + age + ", male=" + male + ", name=" + name + "]";
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        ByteArrayInputStream bais = new ByteArrayInputStream("this is a test !!!".getBytes());
+        Person p1 = new Person("tom", 18, true);
+        Person p2 = new Person("jerry", 16, true);
+        Person p3 = new Person("toodles galore", 15, false);
 
-        DataInputStream dis = new DataInputStream(bais);
-
-        byte[] buffer = new byte[dis.available()];
-
-        dis.read(buffer);
-        dis.close();
-
-        System.out.println(new String(buffer));
+        
     }
 }
