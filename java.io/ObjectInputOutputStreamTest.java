d8237b8d4b7ee224359c2b65a0f4c18254693181
yan <yan@li.com>
2022-05-31 14:06:10 +0200

    add object input output stream test

--- java.io/ObjectInputOutputStreamTest.java
+++ java.io/ObjectInputOutputStreamTest.java
@@ -1,27 +1,45 @@
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 public class ObjectInputOutputStreamTest {
+    private static class Person implements Serializable {
+        private String name;
+        private int age;
+        private boolean male;
+
+        public Person(String name, int age, boolean male) {
+            this.name = name;
+            this.age = age;
+            this.male = male;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [age=" + age + ", male=" + male + ", name=" + name + "]";
+        }
+    }
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        String s = new String("hello");
+        String path = "/home/yan/github/BlogTests/person";
 
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeInt(2);
 
-        ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(new Person("tom", 18, true));
+            oos.writeObject(new Person("jerry", 16, true));
+        }
 
-        oos.writeObject(s);
-        oos.close();
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(path))) {
+            int num = ois.readInt();
 
-        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
-
-        ObjectInputStream ois = new ObjectInputStream(bais);
-
-        String ss = (String) ois.readObject();
-        ois.close();
-
-        System.out.println(ss);
+            for (int i = 0; i < num; i++) {
+                Person p = (Person) ois.readObject();
+                System.out.println(p);
+            }
+        }
     }
 }
