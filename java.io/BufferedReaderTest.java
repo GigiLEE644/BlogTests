2e7463e81c1c58d719a7ffc4ecd584ca62c1cbf1
yan <yan@li.com>
2022-06-02 16:10:06 +0200

    add buffered reader test

--- java.io/BufferedReaderTest.java
+++ java.io/BufferedReaderTest.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.StringReader;
 
 public class BufferedReaderTest {
     public static void main(String[] args) throws IOException {
