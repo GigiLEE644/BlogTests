b5cbc806fb39d4b90f9a8ff6067bcf0514b61794
yan <yan@li.com>
2022-06-02 16:09:38 +0200

    add buffered reader test

--- java.io/BufferedReaderTest.java
+++ java.io/BufferedReaderTest.java
@@ -1,15 +1,14 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringReader;
 
 public class BufferedReaderTest {
     public static void main(String[] args) throws IOException {
-        StringReader sr = new StringReader("hello world");
+        String path = "/home/yan/github/BlogTests/README.md";
 
-        BufferedReader reader = new BufferedReader(sr);
-
-        String s = reader.readLine();
-
-        System.out.println(s);
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            reader.lines().forEach(System.out::println);
+        }
     }
 }
