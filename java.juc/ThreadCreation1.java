5373f91b79292417842eab4bacb054b603cb2eb8
yan-gc <yangc@example.com>
2022-09-01 22:18:56 +0200

    add ThreadCreation1

--- java.juc/ThreadCreation1.java
+++ java.juc/ThreadCreation1.java
@@ -1,7 +1,10 @@
 public class ThreadCreation1 {
     public static void main(String[] args) throws InterruptedException {
-        Runnable r = () -> {
-            System.out.println(Thread.currentThread().getName());
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
         };
 
         Thread t = new Thread(r, "Thread1");
