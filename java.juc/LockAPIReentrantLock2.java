9b467c0ccfd7d8b82a95449a0930e32abb3d0461
yan <yan.li@myblog.com>
2022-10-30 10:58:26 +0100

    add LockAPIReentrantLock2

---
+++ java.juc/LockAPIReentrantLock2.java
@@ -0,0 +1,49 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class LockAPIReentrantLock2 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService service = Executors.newFixedThreadPool(10);
+
+        Counter counter = new Counter();
+
+        for (int i = 0; i < 1000; i++) {
+            service.submit(counter::inc);
+        }
+
+        service.shutdown();
+
+        service.awaitTermination(10, TimeUnit.SECONDS);
+
+        System.out.println("sum = " + counter.sum);
+    }
+
+    private static class Counter {
+        private int sum = 0;
+
+        private Lock lock = new ReentrantLock();
+
+        void inc() {
+            while (true) {
+                if (this.lock.tryLock()) {
+                    try {
+                        this.sum++;
+                        return;
+                    } finally {
+                        this.lock.unlock();
+                    }
+                }
+            }
+        }
+    }
+}
+
+/*
+Output :
+    yan@ubuntu:~/github/BlogTests$  /usr/bin/env /home/yan/Apps/jdk-17.0.3.1/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/yan/.config/Code/User/workspaceStorage/cb6e219fa798ac2300babcaaca8a9a47/redhat.java/jdt_ws/BlogTests_e0c01ebd/bin LockAPIReentrantLock2 
+    sum = 1000
+    yan@ubuntu:~/github/BlogTests$
+*/
