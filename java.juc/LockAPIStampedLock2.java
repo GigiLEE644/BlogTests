aeda678103303a538a6dac9b1fd620cf3b0d18e8
yan <yan.li@myblog.com>
2022-11-11 15:20:46 +0100

    remove comment from lock api stamped lock1 and 2

--- java.juc/LockAPIStampedLock2.java
+++ java.juc/LockAPIStampedLock2.java
@@ -91,11 +91,3 @@ public class LockAPIStampedLock2 {
         }
     }
 }
-
-/*
- StampedLock is an alternative to using a ReadWriteLock (implemented by ReentrantReadWriteLock). The main differences between StampedLock and ReentrantReadWriteLock are that:
-
-StampedLocks allow optimistic locking for read operations
-ReentrantLocks are reentrant (StampedLocks are not)
-So if you have a scenario where you have contention (otherwise you may as well use synchronized or a simple Lock) and more readers than writers, using a StampedLock can significantly improve performance.
- */
