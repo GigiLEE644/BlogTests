87540c70026169b586687ba3438208361b92f235
yan <yan.li@myblog.com>
2022-11-16 21:25:02 +0100

    add LockAPILockSupport2

---
+++ java.juc/LockAPILockSupport2.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class LockAPILockSupport2 {
+    public static void main(String[] args) throws InterruptedException {
+        // get a permit
+        LockSupport.unpark(Thread.currentThread());
+        // consume a permit
+        LockSupport.park();
+        System.out.println("Main thread exiting");
+    }
+}
+
+/*
+ Output :
+    Main thread exiting
+ */
