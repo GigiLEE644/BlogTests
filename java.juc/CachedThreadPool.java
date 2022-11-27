7402be7533160c4cbf90b7f972398dfddeb091f4
yan <yan.li@myblog.com>
2022-11-27 14:34:35 +0100

    add CachedThreadPool

---
+++ java.juc/CachedThreadPool.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) {
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                final Integer taskNumber = i;
+                es.execute(
+                        () -> System.out.println(
+                                Thread.currentThread().getName() + " : executing task " + taskNumber));
+            }
+        } finally {
+            es.shutdown();
+        }
+    }
+}
