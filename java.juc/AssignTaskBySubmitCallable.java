ba073f82ff58079f5b2e064f13757b15bc196750
yan <yan.li@myblog.com>
2022-12-03 14:10:10 +0100

    add AssignTaskBySubmitCallable

---
+++ java.juc/AssignTaskBySubmitCallable.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class AssignTaskBySubmitCallable {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService es = Executors.newFixedThreadPool(3);
+
+        try {
+            List<Future<String>> futures = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                final Integer taskNumber = i;
+
+                futures.add(es.submit(
+                        () -> Thread.currentThread().getName() + " : executing task " + taskNumber));
+            }
+
+            for (int i = 0; i < futures.size(); i++) {
+                System.out.println(futures.get(i).get());
+            }
+        } finally {
+            es.shutdown();
+        }
+    }
+}
