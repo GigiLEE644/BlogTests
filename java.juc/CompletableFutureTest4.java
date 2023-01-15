bc41b6ec6889d58f2957ab5bb907d0b686f5dbc1
yan <yan@li.com>
2023-01-15 16:50:35 +0100

    add CompletableFutureTest4

---
+++ java.juc/CompletableFutureTest4.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureTest4 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<String> f = CompletableFuture
+                .supplyAsync(() -> {
+                    // int result = 1 / 0;
+                    return Thread.currentThread().getName() + " : hello world";
+                })
+                .handle((g, t) -> {
+                    if (t == null) {
+                        return Thread.currentThread().getName() + " : " + g;
+                    }
+
+                    return Thread.currentThread().getName() + " : " + t.getCause().getMessage();
+                });
+
+        // f.completeExceptionally(new RuntimeException("this is an exception"));
+
+        String greeting = f.get();
+
+        System.out.println(greeting);
+    }
+}
