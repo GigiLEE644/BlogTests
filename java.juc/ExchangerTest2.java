7ec3f18450c62dbc67e6d6730dee4193ef6d689c
yan <yan@li.com>
2023-02-25 14:46:28 +0100

    add ExchangerTest2

--- java.juc/ExchangerTest2.java
+++ java.juc/ExchangerTest2.java
@@ -1,36 +1,28 @@
 import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 public class ExchangerTest2 {
-    static Exchanger<String> exchanger = new Exchanger<>();
-
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        Exchanger<String> exchanger = new Exchanger<>();
+        int nThreads = 10;
+        ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
         try {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < nThreads; i++) {
                 executorService.submit(new Runnable() {
                     @Override
                     public void run() {
-                        work();
+                        try {
+                            String name = Thread.currentThread().getName();
+                            String partner = exchanger.exchange(name);
+                            System.out.println("I am thread " + name + " and my partner of exchange is " + partner);
+                        } catch (InterruptedException ie) {
+                        }
                     }
                 });
             }
         } finally {
             executorService.shutdown();
-            executorService.awaitTermination(1, TimeUnit.HOURS);
         }
-    }
-
-    static void work() {
-        String name = Thread.currentThread().getName();
-        String received = "";
-        try {
-            received = exchanger.exchange(name);
-        } catch (InterruptedException ie) {
-            // ignore for now
-        }
-        System.out.println("I am thread " + name + " and I received the string " + received);
     }
 }
