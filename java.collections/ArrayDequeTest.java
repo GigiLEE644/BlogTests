8c1ec84929dda7e892d73edc6b233015b47d7fdf
yan <yan@gigilee.com>
2023-03-24 22:16:24 +0100

    add ArrayDequeTest

--- java.collections/ArrayDequeTest.java
+++ java.collections/ArrayDequeTest.java
@@ -4,28 +4,28 @@ public class ArrayDequeTest {
     public static void main(String[] args) {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
 
-        stack.add(1);
-        stack.add(2);
-        stack.add(3);
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
 
         System.out.println(stack);
 
         while (!stack.isEmpty()) {
-            System.out.println(stack.removeLast());
+            System.out.println(stack.pop());
         }
 
         System.out.println();
 
         ArrayDeque<Integer> queue = stack;
 
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
 
         System.out.println(queue);
 
         while (!queue.isEmpty()) {
-            System.out.println(queue.removeFirst());
+            System.out.println(queue.poll());
         }
     }
 }
