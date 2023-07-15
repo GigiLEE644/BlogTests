dd9834be08cd7640416c684fb9e4798e9f7a096a
gigilee644 <ghub8551@gmail.com>
2023-07-15 11:25:28 +0200

    add jdbc test

---
+++ java.spi/JDBCTest.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JDBCTest1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url = "jdbc:mysql://localhost:3306/students";
+        String username = "gigi";
+        String password = "1234";
+        String query = "select *from students";
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection con = DriverManager.getConnection(url, username, password);
+
+        Statement st = con.createStatement();
+
+        ResultSet rs = st.executeQuery(query);
+
+        rs.next();
+
+        String name = rs.getString("name");
+
+        System.out.println(name);
+
+        st.close();
+        con.close();
+    }
+}
