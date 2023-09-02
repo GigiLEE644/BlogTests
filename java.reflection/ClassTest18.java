78a55425a3bb07480ef5d31ba0075db9c9c877fd
yan <yan.li@myblog.com>
2023-09-02 19:00:34 +0200

    add class test 18

--- java.reflection/ClassTest18.java
+++ java.reflection/ClassTest18.java
@@ -1,10 +1,12 @@
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class ClassTest17 {
+public class ClassTest18 {
     public static void main(String[] args)
             throws SecurityException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
             IllegalAccessException {
+        System.setProperty("java.security.policy",
+                "file:/home/yan/github/BlogTests/java.reflection/my_security.policy");
         System.setSecurityManager(new SecurityManager());
 
         Person p = new Person("tom", 18);
@@ -16,24 +18,5 @@ public class ClassTest17 {
         method.setAccessible(true);
 
         method.invoke(p, "hello");
-    }
-}
-
-class Person {
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Person [name=" + name + ", age=" + age + "]";
-    }
-
-    private void speak(String words) {
-        System.out.println(this + " : " + words);
     }
 }
