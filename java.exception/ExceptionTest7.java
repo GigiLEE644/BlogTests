ac1a97e891227bfa49277e0acff77921d2444cce
yan-gc <yangc@example.com>
2023-10-02 19:45:15 +0200

    add ExceptionTest7

--- java.exception/ExceptionTest7.java
+++ java.exception/ExceptionTest7.java
@@ -1,6 +1,10 @@
 public class ExceptionTest7 {
     public static void main(String[] args) {
         System.out.println(divide(4, 2));
+
+        System.out.println();
+
+        System.out.println(divide(4, 0));
     }
 
     private static int divide(int a, int b) {
@@ -11,9 +15,9 @@ public class ExceptionTest7 {
             return c;
         } catch (ArithmeticException e) {
             e.printStackTrace();
+            return c;
         } finally {
             System.out.println("I am in finally block.");
         }
-        return c;
     }
 }
