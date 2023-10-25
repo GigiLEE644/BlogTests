0173d7f89a7c090fd8f46dd0f9f93bf250ef9a55
gigilee644 <ghub8551@gmail.com>
2023-10-25 22:18:58 +0200

    add ExceptionTest17

--- java.exception/ExceptionTest17.java
+++ java.exception/ExceptionTest17.java
@@ -1,9 +1,13 @@
 public class ExceptionTest17 {
     public static void main(String[] args) {
         try {
-            throw new NullPointerException("This is my null pointer exception");
-        } catch (Exception e) {
+            System.out.println("x");
+            //int x = 1 / 0;
+            throw new NullPointerException();
+        } catch (NullPointerException e) {
+            System.out.println("y");
             e.printStackTrace();
+            int y = 1 / 0;
         } finally {
             System.out.println("In finally.");
         }
