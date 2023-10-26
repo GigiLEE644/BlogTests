8f136b74d9ac5f6ac30362a56c89e52e4ecaf9c2
yan <yan.li@hp2.com>
2023-10-26 22:01:35 +0200

    add ExceptionTest18

--- java.exception/ExceptionTest18.java
+++ java.exception/ExceptionTest18.java
@@ -1,11 +1,11 @@
-public class ExceptionTest17 {
+public class ExceptionTest18 {
     public static void main(String[] args) {
         try {
             System.out.println("In Try.");
-            int x = 1 / 0;
+            Integer.parseInt(" ");
         } catch (ArithmeticException e) {
             System.out.println("In Catch.");
-            Integer.parseInt(" ");
+            int x = 1 / 0;
             e.printStackTrace();
         } finally {
             System.out.println("In finally.");
