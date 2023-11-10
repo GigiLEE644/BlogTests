93d4a706965dc4639c81aad2e35a3e4b0c4fa180
yan <yan.li@hp2.com>
2023-11-10 22:26:14 +0100

    add AnnotationTest3

---
+++ java.annotation/AnnotationTest3.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AnnotationTest3 {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+        myList.add("test");
+    }
+}
