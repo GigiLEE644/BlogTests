949156d0326503422af49fc498b57ccf94fc7dba
yan <yan@gigilee.com>
2023-12-04 13:46:40 +0100

    add GenericsTest7

--- java.generics/GenericsTest7.java
+++ java.generics/GenericsTest7.java
@@ -1,7 +1,10 @@
 public class GenericsTest7 {
     public static void main(String[] args) {
-        System.out.println(min(1, 2));
-        System.out.println(max(1, 2));
+        System.out.println("int min = " + min(1, 2));
+        System.out.println("int max = " + max(1, 2));
+
+        System.out.println("double min = " + min(1d, 2d));
+        System.out.println("double max = " + max(1d, 2d));
     }
 
     private static <T extends Comparable<T>> T min(T a, T b) {
