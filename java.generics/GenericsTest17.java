c6f635f81ff80557e069eeef3d128f261fa876a8
yan <yan.li@hp2.com>
2023-12-17 17:02:47 +0100

    add GenericsTest17

---
+++ java.generics/GenericsTest17.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest17 {
+    public static void main(String[] args) {
+        List elements = new ArrayList();
+    }
+}
