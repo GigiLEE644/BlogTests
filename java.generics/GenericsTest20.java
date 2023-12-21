e2b29bad9c0f3f75a9e83b0bd03a59fd3902c3bf
yan <yan.li@hp2.com>
2023-12-21 21:53:53 +0100

    add GenericsTest20

---
+++ java.generics/GenericsTest20.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsTest20 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+    }
+}
