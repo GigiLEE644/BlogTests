d42a6efd6ba1c0c09a96afccfa8b679dcc361ee2
yan-gc <yangc@example.com>
2024-01-04 21:32:15 +0100

    add Java8NewFeaturesTest3

---
+++ java.features/8/Java8NewFeaturesTest3.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Java8NewFeaturesTest3 {
+    public static void main(String[] args) {
+        Consumer<List<Integer>> modify = l -> {
+            for (int i = 0; i < l.size(); i++) {
+                l.set(i, 2 * l.get(i));
+            }
+        };
+
+        Consumer<List<Integer>> display = l -> {
+            for (int i = 0; i < l.size(); i++) {
+                System.out.print(l.get(i) + " ");
+            }
+            System.out.println();
+        };
+
+        List<Integer> l = new ArrayList<Integer>();
+        
+        l.add(1);
+        l.add(2);
+        l.add(3);
+
+        display.andThen(modify).andThen(display).accept(l);
+    }
+}
