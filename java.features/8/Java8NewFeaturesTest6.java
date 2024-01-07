05c9b6a51aa43d814776b94616230ad5e40d58a6
gigilee644 <ghub8551@gmail.com>
2024-01-07 10:26:34 +0100

    add Java8NewFeaturesTest6

---
+++ java.features/8/Java8NewFeaturesTest6.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Java8NewFeaturesTest6 {
+    public static void main(String[] args) {
+        List<Object[]> users = new ArrayList<>();
+
+        users.add(new Object[] { "tom", 18 });
+        users.add(new Object[] { "jerry", 16 });
+        users.add(new Object[] { "spike", 21 });
+        users.add(new Object[] { "tyke", 6 });
+
+        Function<List<Object[]>, List<Person>> f1 = l -> {
+            List<Person> p = new ArrayList<>();
+
+            for (Object[] o : l) {
+                p.add(new Person((String) o[0], (int) o[1]));
+            }
+
+            return p;
+        };
+
+        Function<List<Person>, List<Person>> f2 = l -> {
+            l.sort((p1, p2) -> {
+                return p1.age > p2.age ? 1 : p1.age == p2.age ? 0 : -1;
+            });
+
+            return l;
+        };
+
+        System.out.println();
+        System.out.println("Original List :");
+
+        for (Person person : f1.apply(users)) {
+            System.out.println(person);
+        }
+
+        System.out.println();
+        System.out.println("Sorted List by using andThen :");
+
+        for (Person person : f1.andThen(f2).apply(users)) {
+            System.out.println(person);
+        }
+
+        System.out.println();
+        System.out.println("Sorted List by using compose :");
+
+        for (Person person : f2.compose(f1).apply(users)) {
+            System.out.println(person);
+        }
+    }
+
+    private static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [name=" + name + ", age=" + age + "]";
+        }
+    }
+}
