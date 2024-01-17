2a2cfd6a4ef503bb43003959f33d2a805ea8b5fc
gigilee644 <ghub8551@gmail.com>
2024-01-17 21:38:17 +0100

    add Java8NewFeaturesTest17

---
+++ java.features/8/Java8NewFeaturesTest17.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Java8NewFeaturesTest17 {
+    public static void main(String[] args) throws IOException {
+        String hello = "hello";
+        String world = "world";
+
+        // Collection
+        List<String> l = Arrays.asList(hello, world);
+
+        Stream<String> ls = l.stream();
+
+        ls.forEach(System.out::println);
+
+        System.out.println();
+
+        // Array
+        String[] s = { hello, world };
+
+        Stream<String> as = Arrays.stream(s);
+
+        as.forEach(System.out::println);
+
+        System.out.println();
+
+        // File
+        File f = File.createTempFile(hello, world);
+
+        f.deleteOnExit();
+
+        Path p = f.toPath();
+        Files.write(p, hello.getBytes(), StandardOpenOption.APPEND);
+        Files.write(p, System.lineSeparator().getBytes(), StandardOpenOption.APPEND);
+        Files.write(p, world.getBytes(), StandardOpenOption.APPEND);
+
+        try (Stream<String> fs = Files.lines(p)) {
+            fs.forEach(System.out::println);
+        }
+    }
+}
