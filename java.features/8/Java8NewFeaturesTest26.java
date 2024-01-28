64f3971949c35de3498412a7f524f51c43bef2c5
gigilee644 <ghub8551@gmail.com>
2024-01-28 09:03:49 +0100

    add Java8NewFeaturesTest26

---
+++ java.features/8/Java8NewFeaturesTest26.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Java8NewFeaturesTest26 {
+    public static void main(String[] args) {
+        Person p1 = new Person("tom", 18);
+        Person p2 = new Person("jerry", 16);
+        Person p3 = new Person("spike", 26);
+        Person p4 = new Person("tyke", 6);
+
+        List<Person> ps = new ArrayList<>();
+
+        ps.add(p1);
+        ps.add(p2);
+        ps.add(p3);
+        ps.add(p4);
+
+        Stream<Person> s = ps.stream()
+                .filter(p -> p.age > 10)
+                .peek(p -> {
+                    System.out.println("peek : " + p);
+                })
+                .limit(2);
+
+        s.forEach(System.out::println);
+    }
+
+    private static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person [name=" + name + ", age=" + age + "]";
+        }
+    }
+}
