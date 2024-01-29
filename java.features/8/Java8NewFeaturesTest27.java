348e1f283e630162cad872055a3e052761745a31
yan <yan@yan.com>
2024-01-29 10:27:26 +0100

    add Java8NewFeaturesTest27

---
+++ java.features/8/Java8NewFeaturesTest27.java
@@ -0,0 +1,32 @@
+import java.util.stream.Stream;
+
+public class Java8NewFeaturesTest27 {
+    public static void main(String[] args) {
+        Integer[] arr = { 1, 3, 4, 2 };
+
+        sort(Stream.of(arr)).forEach(System.out::println);
+
+        System.out.println();
+
+        sort(Stream.of(arr).sequential()).forEach(System.out::println);
+
+        System.out.println();
+
+        sort(Stream.of(arr).parallel()).forEach(System.out::println);
+
+        System.out.println();
+
+        sort(Stream.of(arr).parallel().sequential()).forEach(System.out::println);
+
+        System.out.println();
+
+        sort(Stream.of(arr).parallel()).sequential().forEach(System.out::println);
+    }
+
+    private static Stream<Integer> sort(Stream<Integer> s) {
+        return s.sorted()
+                .peek(i -> {
+                    System.out.println(Thread.currentThread().getName() + " : " + i);
+                });
+    }
+}
