ad301b6eaf4bcffb9b5a452a3c37416e7db1b936
gigilee644 <ghub8551@gmail.com>
2024-02-13 19:59:42 +0100

    add Java8NewFeaturesTest45

---
+++ java.features/8/Java8NewFeaturesTest45.java
@@ -0,0 +1,20 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Java8NewFeaturesTest45 {
+    public static void main(String[] args) {
+        String word = Stream.of("he", "ll", "o").collect(Collectors.joining());
+
+        System.out.println(word);
+
+        String[] words = { "hello", "world" };
+
+        String phrase = Stream.of(words).collect(Collectors.joining(" "));
+
+        System.out.println(phrase);
+
+        String greeting = Stream.of(words).collect(Collectors.joining(" ", "tom : ", "!"));
+
+        System.out.println(greeting);
+    }
+}
