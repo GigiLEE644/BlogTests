cb539db249369ab2e8d9c50398abd6edad13c333
yan-gc <yangc@example.com>
2024-03-11 16:48:49 +0100

    modify Java8NewFeaturesTest57

--- java.features/8/Java8NewFeaturesTest57.java
+++ java.features/8/Java8NewFeaturesTest57.java
@@ -3,18 +3,22 @@ import java.util.stream.Stream;
 
 public class Java8NewFeaturesTest57 {
     public static void main(String[] args) {
-        IntStream.range(1, 6).forEach(System.out::println);
+        IntStream.range(1, 5).forEach(System.out::println);
 
         System.out.println();
 
-        IntStream.rangeClosed(1, 6).forEach(System.out::println);
+        IntStream.rangeClosed(1, 5).forEach(System.out::println);
 
         System.out.println();
 
-        System.out.println(Stream.of("1", "2", "3", "4").mapToInt(Integer::parseInt).sum());
+        Stream.of("1", "2", "3", "4").mapToInt(Integer::parseInt).forEach(System.out::println);
 
         System.out.println();
 
-        System.out.println(IntStream.range(1, 5).mapToObj(i -> "" + i).mapToInt(Integer::parseInt).sum());
+        IntStream.range(1, 5).mapToObj(i -> "" + i).forEach(System.out::println);
+
+        System.out.println();
+
+        IntStream.range(1, 5).boxed().forEach(System.out::println);
     }
 }
