83895fc63b59d17151e8a16ea3b7543c282663aa
yan-gc <yangc@example.com>
2024-03-25 14:52:45 +0100

    modify Java8NewFeaturesTest68 69 70

--- java.features/8/Java8NewFeaturesTest69.java
+++ java.features/8/Java8NewFeaturesTest69.java
@@ -1,10 +1,5 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Java8NewFeaturesTest69 {
     public static void main(String[] args) {
--- java.features/8/Java8NewFeaturesTest70.java
+++ java.features/8/Java8NewFeaturesTest70.java
@@ -1,12 +1,8 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Java8NewFeaturesTest70 {
     public static void main(String[] args) {
