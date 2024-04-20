56c356cbb5687fafc63489a12c1b3d5863ca4a8f
yan <yan@yan.com>
2024-04-20 11:20:52 +0200

    modify Java8NewFeaturesTest83

--- java.features/8/Java8NewFeaturesTest83.java
+++ java.features/8/Java8NewFeaturesTest83.java
@@ -5,19 +5,19 @@ public class Java8NewFeaturesTest83 {
         Optional<String> opt1 = Optional.of("tom");
 
         System.out.println(opt1.isPresent());
-        System.out.println(opt1.isEmpty());
+        System.out.println(!opt1.isEmpty());
         System.out.println();
 
         Optional<String> opt2 = Optional.ofNullable(null);
 
         System.out.println(opt2.isPresent());
-        System.out.println(opt2.isEmpty());
+        System.out.println(!opt2.isEmpty());
         System.out.println();
 
         Optional<Object> opt3 = Optional.empty();
 
         System.out.println(opt3.isPresent());
-        System.out.println(opt3.isEmpty());
+        System.out.println(!opt3.isEmpty());
         System.out.println();
     }
 }
