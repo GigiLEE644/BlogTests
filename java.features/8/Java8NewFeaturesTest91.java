e2cd56a90f22f0436e70a36c1272b0c7d0a5745d
gigilee644 <ghub8551@gmail.com>
2024-04-28 09:35:17 +0200

    add Java8NewFeaturesTest91

---
+++ java.features/8/Java8NewFeaturesTest91.java
@@ -0,0 +1,50 @@
+import java.util.Optional;
+
+public class Java8NewFeaturesTest91 {
+    public static void main(String[] args) {
+        Optional<Computer> computer = Optional.ofNullable(new Computer(new MotherBoard(new Memory(64))));
+
+        // Integer capacity = computer.map(Computer::getMotherBoard).map(MotherBoard::getMemory).map(Memory::getCapacity).orElse(0);
+
+        Integer capacity = computer.flatMap(Computer::getMotherBoard).flatMap(MotherBoard::getMemory)
+                .map(Memory::getCapacity).orElse(0);
+
+        System.out.println(capacity);
+    }
+
+    private static class Computer {
+        private Optional<MotherBoard> motherBoard;
+
+        public Computer(MotherBoard motherBoard) {
+            this.motherBoard = Optional.ofNullable(motherBoard);
+        }
+
+        public Optional<MotherBoard> getMotherBoard() {
+            return motherBoard;
+        }
+    }
+
+    private static class MotherBoard {
+        private Optional<Memory> memory;
+
+        public MotherBoard(Memory memory) {
+            this.memory = Optional.ofNullable(memory);
+        }
+
+        public Optional<Memory> getMemory() {
+            return memory;
+        }
+    }
+
+    private static class Memory {
+        private int capacity;
+
+        public Memory(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public int getCapacity() {
+            return capacity;
+        }
+    }
+}
