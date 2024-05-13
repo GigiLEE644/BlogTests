3c3115194f7e86c1a7ceeb7077b4492eca4a22e4
yan <yan@yan.com>
2024-05-13 19:47:37 +0200

    add Java8NewFeaturesTest99

---
+++ java.features/8/Java8NewFeaturesTest99.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Java8NewFeaturesTest99 {
+    public static void main(String[] args) {
+        Date d = new Date();
+
+        System.out.println(d);
+
+        Calendar c = Calendar.getInstance();
+
+        System.out.println(c.getTime());
+
+        System.out.println();
+
+        LocalDateTime ldt1 = LocalDateTime.ofInstant(d.toInstant(), ZoneId.systemDefault());
+
+        System.out.println(ldt1);
+
+        LocalDateTime ldt2 = LocalDateTime.ofInstant(c.toInstant(), ZoneId.systemDefault());
+
+        System.out.println(ldt2);
+    }
+}
