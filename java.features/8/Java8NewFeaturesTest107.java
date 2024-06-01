110cdccefa7958b056f96c67c4822e6cef899356
gigilee644 <ghub8551@gmail.com>
2024-06-01 10:00:44 +0200

    modify Java8NewFeaturesTest107

--- java.features/8/Java8NewFeaturesTest107.java
+++ java.features/8/Java8NewFeaturesTest107.java
@@ -15,10 +15,30 @@ public class Java8NewFeaturesTest107 {
 
         System.out.println(sj2.toString());
 
-        StringJoiner sj3 = new StringJoiner(delimiter);
+        StringJoiner sj3 = new StringJoiner(delimiter, prefix, suffix);
 
         sj3.setEmptyValue("defaultValue");
 
         System.out.println(sj3.toString());
+
+        StringJoiner sj4 = new StringJoiner(delimiter);
+
+        sj4.setEmptyValue("defaultValue");
+
+        System.out.println(sj4.toString());
+
+        StringJoiner sj5 = new StringJoiner(delimiter, prefix, suffix);
+
+        sj5.setEmptyValue("defaultValue");
+        sj5.add("hello");
+
+        System.out.println(sj5.toString());
+
+        StringJoiner sj6 = new StringJoiner(delimiter);
+
+        sj6.setEmptyValue("defaultValue");
+        sj6.add("world");
+
+        System.out.println(sj6.toString());
     }
 }
