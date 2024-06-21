4abdd68bfe96ba7ea7e53c43bb3c32815811a71f
yan <yan@yan.com>
2024-06-21 22:08:53 +0200

    add Java11NewFeaturesTest8

---
+++ java.features/11/Java11NewFeaturesTest8.java
@@ -0,0 +1,40 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Java11NewFeaturesTest8 {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+        try {
+            HttpClient httpClient = HttpClient.newBuilder().executor(executorService)
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .connectTimeout(Duration.ofSeconds(10)).build();
+
+            HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create("https://www.google.fr/")).build();
+
+            CompletableFuture<HttpResponse<String>> responseFuture = httpClient.sendAsync(request,
+                    HttpResponse.BodyHandlers.ofString());
+
+            responseFuture.thenAccept(response -> {
+                response.headers().map().forEach((k, v) -> System.out.println(k + ":" + v));
+
+                System.out.println();
+
+                System.out.println(response.statusCode());
+
+                System.out.println();
+
+                System.out.println(response.body());
+            }).join();
+        } finally {
+            executorService.shutdown();
+        }
+    }
+}
