2f9e3004bee2d9822ca255f31706a1a6f5431ca5
yan <yan@yan.com>
2024-07-04 22:52:51 +0200

    add Computer class

---
+++ java.features/11/run.java.file/Computer.java
@@ -0,0 +1,12 @@
+class Screen {
+        public static void main(String[] args) {
+                System.out.println("I am a screen");
+        }
+}
+
+public class Computer {
+
+        public static void main(String[] args) {
+                System.out.println("I am a computer");
+        }
+}
