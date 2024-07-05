926565ab700b0964e8b465d636a470ce6d3e4157
gigili <gigili@gigiyan.com>
2024-07-05 11:52:09 +0200

    add Car.java

---
+++ java.features/11/run.java.file/Car.java
@@ -0,0 +1,14 @@
+class Engine {
+        /*
+         * public static void main(String[] args) {
+         * System.out.println("I am an engine");
+         * }
+         */
+}
+
+public class Car {
+
+        public static void main(String[] args) {
+                System.out.println("I am a car");
+        }
+}
