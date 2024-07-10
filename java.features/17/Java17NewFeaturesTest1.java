6ea1f7a15ed2288ba260c6fd548e11f5fdb1e9b9
gigilee644 <ghub8551@gmail.com>
2024-07-10 21:24:38 +0200

    add Java17NewFeaturesTest1

---
+++ java.features/17/Java17NewFeaturesTest1.java
@@ -0,0 +1,12 @@
+public class Java17NewFeaturesTest1 {
+
+    public static void main(String[] args) {
+        String person = """
+                {
+                    "name": "tom",
+                    "age": 18
+                }
+                """;
+        System.out.println(person);
+    }
+}
