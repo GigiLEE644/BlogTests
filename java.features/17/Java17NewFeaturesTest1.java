cc964d588eceef99b235ac1a1b5243406c7ff3e9
gigilee644 <ghub8551@gmail.com>
2024-07-11 20:53:02 +0200

    modify Java17NewFeaturesTest1

--- java.features/17/Java17NewFeaturesTest1.java
+++ java.features/17/Java17NewFeaturesTest1.java
@@ -7,6 +7,19 @@ public class Java17NewFeaturesTest1 {
                     "age": 18
                 }
                 """;
+
         System.out.println(person);
+
+        String escaped = """
+                Text blocks start with a \""" (three double-quote marks) followed by optional whitespaces and a newline.
+                """;
+
+        System.out.println(escaped);
+
+        String formated = """
+                hi, my name is %s.
+                """.formatted("tom");
+
+        System.out.println(formated);
     }
 }
