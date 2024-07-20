2ce2961dbb8ad1117954f84fafac7baf6d8d2588
yan <yan@yan.com>
2024-07-20 10:05:29 +0200

    modify Java17NewFeaturesTest5

--- java.features/17/Java17NewFeaturesTest5.java
+++ java.features/17/Java17NewFeaturesTest5.java
@@ -6,20 +6,6 @@ public class Java17NewFeaturesTest5 {
                 System.out.println(tom);
                 System.out.println(tom.name());
                 System.out.println(tom.age());
-
-                System.out.println();
-
-                Person tom2 = new Person("tom", 18);
-
-                System.out.println(tom == tom2);
-                System.out.println(tom.equals(tom2));
-
-                System.out.println();
-
-                Person jerry = new Person("jerry", 16);
-
-                System.out.println(tom == jerry);
-                System.out.println(tom.equals(jerry));
         }
 
         private static record Person(String name, int age) {
