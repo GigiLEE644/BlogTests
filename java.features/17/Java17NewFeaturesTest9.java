1f00ae2385b83e6af89e2e5d067a41b734d4e17a
yan <yan@yan.com>
2024-07-27 16:46:31 +0200

    add Java17NewFeaturesTest9

---
+++ java.features/17/Java17NewFeaturesTest9.java
@@ -0,0 +1,37 @@
+public class Java17NewFeaturesTest9 {
+
+        public static void main(String[] args) {
+                try {
+                        new Person("", 18);
+                } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                }
+
+                System.out.println();
+
+                try {
+                        new Person("tom", 0);
+                } catch (IllegalArgumentException e) {
+                        e.printStackTrace();
+                }
+
+                System.out.println();
+
+                Person tom = new Person("tom", 18);
+
+                System.out.println(tom);
+
+        }
+
+        private static record Person(String name, int age) {
+                Person {
+                        if (name == null || name.trim().isEmpty()) {
+                                throw new IllegalArgumentException("name is required");
+                        }
+
+                        if (age <= 0) {
+                                throw new IllegalArgumentException("age should >= 1");
+                        }
+                }
+        }
+}
