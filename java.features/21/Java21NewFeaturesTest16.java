e7c71a70a59e0279f478bf5a69a9e31dc6847052
yan-gc <yangc@example.com>
2024-08-24 19:45:36 +0200

    add Java21NewFeaturesTest16

---
+++ java.features/21/Java21NewFeaturesTest16.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Java21NewFeaturesTest16 {
+
+        public static void main(String[] args) throws IOException, InterruptedException {
+                ProcessBuilder builder = new ProcessBuilder();
+
+                builder.command("ls", "-l", "/tmp");
+
+                Path temp = Files.createTempFile("hello", "world");
+
+                builder.redirectOutput(temp.toFile());
+
+                Process process = builder.start();
+
+                process.waitFor();
+
+                try {
+                        String output = new String(Files.readAllBytes(temp));
+
+                        System.out.println(output);
+                } finally {
+                        Files.deleteIfExists(temp);
+                }
+        }
+}
