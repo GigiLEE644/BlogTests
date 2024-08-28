d9e4923d5c52323251f7b28fb6654380accec9cc
gigilee644 <ghub8551@gmail.com>
2024-08-28 18:16:22 +0200

    add BitOperationsTest1

---
+++ java.miscellaneous/BitOperationsTest1.java
@@ -0,0 +1,27 @@
+public class BitOperationsTest1 {
+    public static void main(String[] args) {
+        int a = 0b00000000000000000000000000001001;
+
+        System.out.println(a);
+
+        int b = 000000000011;
+
+        System.out.println(b);
+
+        int c = 0x00000009;
+
+        System.out.println(c);
+
+        int aa = 0b11111111111111111111111111110111;
+
+        System.out.println(aa);
+
+        int bb = 037777777767;
+
+        System.out.println(bb);
+
+        int cc = 0xFFFFFFF7;
+
+        System.out.println(cc);
+    }
+}
