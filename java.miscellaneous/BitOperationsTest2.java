701ac3a8d3d29fd0a038c4fd82a19e53c4bba40c
gigilee644 <ghub8551@gmail.com>
2024-08-29 10:52:15 +0200

    add BitOperationsTest2

---
+++ java.miscellaneous/BitOperationsTest2.java
@@ -0,0 +1,27 @@
+public class BitOperationsTest2 {
+    public static void main(String[] args) {
+        int a = 2;
+
+        System.out.println(a);
+
+        int aa = -a;
+
+        System.out.println(aa);
+
+        int aaa = ~a + 1;
+
+        System.out.println(aaa);
+
+        int b = Integer.MIN_VALUE;
+
+        System.out.println(b);
+
+        int bb = -b;
+
+        System.out.println(bb);
+
+        int bbb = ~b + 1;
+
+        System.out.println(bbb);
+    }
+}
