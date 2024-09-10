fb06727e523ec3fa08f5c3ec28e713debecdbc0f
gigili <gigili@gigiyan.com>
2024-09-10 15:30:06 +0200

    modify BitOperationsTest16

--- java.miscellaneous/BitOperationsTest16.java
+++ java.miscellaneous/BitOperationsTest16.java
@@ -1,17 +1,21 @@
 public class BitOperationsTest16 {
     public static void main(String[] args) {
-        System.out.println(is2n(1));
-        System.out.println(is2n(2));
-        System.out.println(is2n(3));
-        System.out.println(is2n(4));
-        System.out.println(is2n(5));
-        System.out.println(is2n(6));
-        System.out.println(is2n(7));
-        System.out.println(is2n(8));
-        System.out.println(is2n(9));
+        System.out.println(isPowerOf2(1));
+        System.out.println(isPowerOf2(2));
+        System.out.println(isPowerOf2(3));
+        System.out.println(isPowerOf2(4));
+        System.out.println(isPowerOf2(5));
+        System.out.println(isPowerOf2(6));
+        System.out.println(isPowerOf2(7));
+        System.out.println(isPowerOf2(8));
+        System.out.println(isPowerOf2(9));
     }
 
-    private static boolean is2n(int a) {
+    private static boolean isPowerOf2(int a) {
+        if(a < 0) {
+            return false;
+        }
+        
         return a == oneAtTheMostRightSide(a);
     }
 
