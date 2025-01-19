eb9aeb9debda8f8e0f79fa6929828916b779037d
gigilee644 <ghub8551@gmail.com>
2025-01-19 17:53:31 +0100

    add ChannelAndBuffer

---
+++ java.nio/ChannelAndBuffer.java
@@ -0,0 +1,70 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class ChannelAndBuffer {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // echo -e "123456789\c" > numbers
+        String path = "/home/yan/github/BlogTests/numbers";
+
+        try (FileInputStream fis = new FileInputStream(path);) {
+            FileChannel channel = fis.getChannel();
+
+            int bufferSize = 8;
+
+            if (bufferSize > channel.size()) {
+                bufferSize = (int) channel.size();
+            }
+
+            ByteBuffer buffer = ByteBuffer.allocate(bufferSize);
+
+            info(buffer);
+
+            read(channel, buffer);
+
+            info(buffer);
+
+            show(buffer);
+
+            clear(buffer);
+
+            info(buffer);
+
+            show(buffer);
+
+            read(channel, buffer);
+
+            info(buffer);
+
+            show(buffer);
+        }
+    }
+
+    private static void clear(ByteBuffer buffer) {
+        System.out.println();
+        System.out.println("clear buffer");
+        buffer.clear();
+    }
+
+    private static void read(FileChannel channel, ByteBuffer buffer) throws IOException {
+        System.out.println();
+        System.out.println("read " + channel.read(buffer) + " bytes to buffer");
+    }
+
+    private static void show(ByteBuffer buffer) {
+        System.out.println();
+        System.out.println("buffer content = " + new String(buffer.array()));
+    }
+
+    private static void info(ByteBuffer buffer) {
+        System.out.println();
+        System.out.println("----------------------------------------");
+        System.out.println("buffer capacity = " + buffer.capacity());
+        System.out.println("buffer position = " + buffer.position());
+        System.out.println("buffer limit = " + buffer.limit());
+        System.out.println("buffer remaining = limit - position = " + buffer.remaining());
+        System.out.println("----------------------------------------");
+    }
+}
