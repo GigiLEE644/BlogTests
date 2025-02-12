1022ccd034424ed739880281ece33f98a609e803
gigilee644 <ghub8551@gmail.com>
2025-02-12 20:51:46 +0100

    add bit operations test 19

---
+++ java.miscellaneous/BitOperationsTest19.java
@@ -0,0 +1,17 @@
+public class BitOperationsTest19 {
+    public static void main(String[] args) {
+        int a = 4;
+
+        System.out.println(a);
+
+        System.out.println();
+
+        System.out.println(a << 1);
+        System.out.println(a << 2);
+
+        System.out.println();
+
+        System.out.println(a >> 1);
+        System.out.println(a >> 2);
+    }
+}
