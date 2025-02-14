c5115fa8d4b460f960b0fca25c9cdbcaf209616d
gigilee644 <ghub8551@gmail.com>
2025-02-14 16:58:26 +0100

    add BitOperationsTest21

---
+++ java.miscellaneous/BitOperationsTest21.java
@@ -0,0 +1,26 @@
+public class BitOperationsTest21 {
+    public static void main(String[] args) {
+        int[] arr = { 10, 9 };
+
+        System.out.println("arr[0] = " + arr[0]);
+        System.out.println("arr[1] = " + arr[1]);
+
+        swap(arr, 0, 1);
+
+        System.out.println("arr[0] = " + arr[0]);
+        System.out.println("arr[1] = " + arr[1]);
+
+        swap(arr, 0, 0);
+
+        System.out.println("arr[0] = " + arr[0]);
+        System.out.println("arr[1] = " + arr[1]);
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        if (i != j) {
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
+    }
+}
