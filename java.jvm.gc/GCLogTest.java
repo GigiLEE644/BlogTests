5e5904fb5a592aa05791dff1bb12ceacf683a7b8
gigilee644 <ghub8551@gmail.com>
2025-07-18 09:16:34 +0200

    add other gc configs

--- java.jvm.gc/GCLogTest.java
+++ java.jvm.gc/GCLogTest.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * -XX:+UseParallelGC
  * -verbose:gc
- * -XX:+PrintGC
- * -XX:+PrintGCDetails
+ * -XX:+PrintGC or -Xlog:gc
+ * -XX:+PrintGCDetails or -Xlog:gc*
  * -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps
  */
 public class GCLogTest {
     public static void main(String[] args) {
         List<byte[]> list = new ArrayList<>();
 
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 5000; i++) {
             list.add(new byte[1024 * 100]); // 100KB
         }
     }
