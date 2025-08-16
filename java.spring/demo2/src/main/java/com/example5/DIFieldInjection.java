65143172e21da97553de67b1e1d07bbd7fde8ce6
yan <yan@yan.com>
2025-08-16 21:59:39 +0200

    add DIFieldInjection

---
+++ java.spring/demo2/src/main/java/com/example5/DIFieldInjection.java
@@ -0,0 +1,36 @@
+package com.example5;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+
+@ComponentScan
+public class DIFieldInjection {
+    @Service
+    private static class PaymentService {
+        public void processPayment() {
+            System.out.println("Processing payment...");
+        }
+    }
+
+    @Service
+    private static class OrderService {
+
+        // Field injection
+        @Autowired
+        private PaymentService paymentService;
+
+        public void placeOrder() {
+            System.out.println("Placing order...");
+            paymentService.processPayment();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DIFieldInjection.class)) {
+            OrderService orderService = context.getBean(OrderService.class);
+            orderService.placeOrder();
+        }
+    }
+}
