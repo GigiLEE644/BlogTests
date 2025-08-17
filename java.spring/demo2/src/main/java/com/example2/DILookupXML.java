30be4a3d76bb36c5080c256f2e925a09ec0dd9a0
gigilee644 <ghub8551@gmail.com>
2025-08-17 12:16:10 +0200

    add DILookupXML and DILookupAnnotation

--- java.spring/demo2/src/main/java/com/example2/DILookupXML.java
+++ java.spring/demo2/src/main/java/com/example2/DILookupXML.java
@@ -1,15 +1,8 @@
-package com.example6;
+package com.example2;
 
-import org.springframework.beans.factory.annotation.Lookup;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Scope;
-import org.springframework.stereotype.Service;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@ComponentScan
-public class DILookup {
-    @Service
-    @Scope("prototype")
+public class DILookupXML {
     private static class PaymentService {
         public PaymentService() {
             System.out.println("New PaymentService created: " + this);
@@ -20,23 +13,23 @@ public class DILookup {
         }
     }
 
-    @Service
     private static abstract class OrderService {
         public OrderService() {}
-
-        // Abstract lookup method – Spring will override this
-        @Lookup
+        
+        // abstract method for lookup
         protected abstract PaymentService getPaymentService();
 
         public void placeOrder() {
             System.out.println("Placing order...");
-            PaymentService paymentService = getPaymentService(); // always new instance
+            PaymentService paymentService = getPaymentService();
             paymentService.processPayment();
         }
     }
 
     public static void main(String[] args) {
-        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DILookup.class)) {
+        try (ClassPathXmlApplicationContext context =
+                     new ClassPathXmlApplicationContext("beans4.xml")) {
+
             OrderService orderService = context.getBean(OrderService.class);
 
             orderService.placeOrder();
