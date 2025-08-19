f06d6427fff31ed571f7101ecfbf5a12fa3e4ea4
gigilee644 <ghub8551@gmail.com>
2025-08-19 09:40:47 +0200

    add DIObjectFactory

---
+++ java.spring/demo2/src/main/java/com/example7/DIObjectFactory.java
@@ -0,0 +1,47 @@
+package com.example7;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class DIObjectFactory {
+
+    @Component
+    private static class OrderService {
+
+        private final ObjectFactory<PaymentService> paymentServiceFactory;
+
+        @Autowired
+        public OrderService(ObjectFactory<PaymentService> paymentServiceFactory) {
+            this.paymentServiceFactory = paymentServiceFactory;
+        }
+
+        public void placeOrder() {
+            PaymentService paymentService = paymentServiceFactory.getObject(); // fresh instance each time
+            paymentService.processPayment();
+        }
+    }
+
+    @Component
+    @Scope("prototype") // Prototype scope
+    private static class PaymentService {
+        public void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                DIObjectFactory.class)) {
+            OrderService orderService = context.getBean(OrderService.class);
+
+            orderService.placeOrder();
+            orderService.placeOrder();
+            orderService.placeOrder();
+        }
+    }
+}
