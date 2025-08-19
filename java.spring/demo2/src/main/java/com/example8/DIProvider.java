dae215dcc4c9a0b68e18734980ab5e8fd51f4da1
gigilee644 <ghub8551@gmail.com>
2025-08-19 09:52:07 +0200

    add DIProvider

---
+++ java.spring/demo2/src/main/java/com/example8/DIProvider.java
@@ -0,0 +1,46 @@
+package com.example8;
+
+import jakarta.inject.Provider;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class DIProvider {
+
+    @Component
+    private static class OrderService {
+
+        private final Provider<PaymentService> paymentServiceProvider;
+
+        public OrderService(Provider<PaymentService> paymentServiceProvider) {
+            this.paymentServiceProvider = paymentServiceProvider;
+        }
+
+        public void placeOrder() {
+            PaymentService paymentService = paymentServiceProvider.get(); // fresh instance
+            paymentService.processPayment();
+        }
+    }
+
+    @Component
+    @Scope("prototype") // Prototype scope
+    private static class PaymentService {
+        public void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    public static void main(String[] args) {
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                DIProvider.class)) {
+            OrderService orderService = context.getBean(OrderService.class);
+
+            orderService.placeOrder();
+            orderService.placeOrder();
+            orderService.placeOrder();
+        }
+    }
+}
