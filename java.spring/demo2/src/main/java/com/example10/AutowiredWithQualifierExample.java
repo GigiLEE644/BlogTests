014b430f7d754868cce130bba2c69ddceb4de084
gigilee644 <ghub8551@gmail.com>
2025-08-22 17:35:43 +0200

    add autowired examples

---
+++ java.spring/demo2/src/main/java/com/example10/AutowiredWithQualifierExample.java
@@ -0,0 +1,43 @@
+package com.example10;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class AutowiredWithQualifierExample {
+private static class PaymentService {
+        protected void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    @Component("PayPal")
+    private static class PaypalPaymentService extends PaymentService {
+    }
+
+    @Component("CreditCard")
+    private static class CreditCardPaymentService extends PaymentService {
+    }
+
+    @Component
+    private static class OrderService {
+        @Autowired
+        @Qualifier("CreditCard")
+        private PaymentService paymentService;
+
+        public void placeOrder() {
+            paymentService.processPayment();
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AutowiredWithQualifierExample.class);
+        OrderService service = context.getBean(OrderService.class);
+        service.placeOrder();
+        context.close();
+    }
+}
---
+++ java.spring/demo2/src/main/java/com/example9/AutowiredWithPrimaryExample.java
@@ -0,0 +1,43 @@
+package com.example9;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class AutowiredWithPrimaryExample {
+    private static class PaymentService {
+        protected void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    @Component
+    private static class PaypalPaymentService extends PaymentService {
+    }
+
+    @Component
+    @Primary
+    private static class CreditCardPaymentService extends PaymentService {
+    }
+
+    @Component
+    private static class OrderService {
+        @Autowired
+        private PaymentService paymentService;
+
+        public void placeOrder() {
+            paymentService.processPayment();
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AutowiredWithPrimaryExample.class);
+        OrderService service = context.getBean(OrderService.class);
+        service.placeOrder();
+        context.close();
+    }
+}
