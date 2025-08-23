ceca2fc21c05ebbfd2448f20e30a84beacd0c530
gigilee644 <ghub8551@gmail.com>
2025-08-23 19:15:19 +0200

    add AutowiredFallBackExample

---
+++ java.spring/demo2/src/main/java/com/example11/AutowiredFallBackExample.java
@@ -0,0 +1,42 @@
+package com.example11;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class AutowiredFallBackExample {
+    @Component("paymentService")
+    private static class PaymentService {
+        protected void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    @Component
+    private static class PaypalPaymentService extends PaymentService {
+    }
+
+    @Component
+    private static class CreditCardPaymentService extends PaymentService {
+    }
+
+    @Component
+    private static class OrderService {
+        @Autowired
+        private PaymentService paymentService;
+
+        public void placeOrder() {
+            paymentService.processPayment();
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                AutowiredFallBackExample.class);
+        OrderService service = context.getBean(OrderService.class);
+        service.placeOrder();
+        context.close();
+    }
+}
