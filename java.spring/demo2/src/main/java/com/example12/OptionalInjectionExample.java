59b8f03f51e1ae27ada0dc8bbe03d17add03a77a
gigilee644 <ghub8551@gmail.com>
2025-08-24 18:57:07 +0200

    add OptionalInjectionExample

---
+++ java.spring/demo2/src/main/java/com/example12/OptionalInjectionExample.java
@@ -0,0 +1,34 @@
+package com.example12;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+public class OptionalInjectionExample {
+    //@Component
+    private static class PaymentService {
+        protected void processPayment() {
+            System.out.println("Processing payment with instance: " + this);
+        }
+    }
+
+    @Component
+    private static class OrderService {
+        @Autowired(required = false)
+        private PaymentService paymentService;
+
+        public void placeOrder() {
+            paymentService.processPayment();
+        }
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+                OptionalInjectionExample.class);
+        OrderService service = context.getBean(OrderService.class);
+        service.placeOrder();
+        context.close();
+    }
+}
