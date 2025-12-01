3983fcb3325648a94f1ef92f46b07502ccbb2936
ovo <ghub8551@gmail.com>
2025-12-01 08:55:33 +0100

    add output

--- java.spring/demo2/src/main/java/com/example19/BeanLifeCycleExample4.java
+++ java.spring/demo2/src/main/java/com/example19/BeanLifeCycleExample4.java
@@ -166,3 +166,43 @@ public class BeanLifeCycleExample4 {
         ctx.close();
     }
 }
+/*
+Output:
+    1.InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation called for: address
+    
+    Address constructor called
+    
+    2.InstantiationAwareBeanPostProcessor#postProcessAfterInstantiation called for: address
+    
+    3.InstantiationAwareBeanPostProcessor#postProcessProperties called for: address
+    Current PropertyValues: PropertyValues: length=1; bean property 'city'
+    
+    setCity called with: New York
+    
+    BeanNameAware#setBeanName called: address
+    
+    BeanFactoryAware#setBeanFactory called: class org.springframework.beans.factory.support.DefaultListableBeanFactory
+    
+    ApplicationContextAware#setApplicationContext called: class org.springframework.context.annotation.AnnotationConfigApplicationContext
+    
+    1.InstantiationAwareBeanPostProcessor#postProcessBeforeInstantiation called for: user
+    
+    User constructor called
+    
+    2.InstantiationAwareBeanPostProcessor#postProcessAfterInstantiation called for: user
+    
+    3.InstantiationAwareBeanPostProcessor#postProcessProperties called for: user
+    Current PropertyValues: PropertyValues: length=2; bean property 'name'; bean property 'address'
+    
+    setName called with: Alice
+    
+    setAddress called with: Address{city='New York'}
+    
+    BeanNameAware#setBeanName called: user
+    
+    BeanFactoryAware#setBeanFactory called: class org.springframework.beans.factory.support.DefaultListableBeanFactory
+    
+    ApplicationContextAware#setApplicationContext called: class org.springframework.context.annotation.AnnotationConfigApplicationContext
+    
+    User [name=Alice, address=Address{city='New York'}]
+*/
