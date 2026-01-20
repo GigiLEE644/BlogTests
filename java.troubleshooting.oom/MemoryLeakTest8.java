044487cea762e76d1bf00989023cdd1b307c4ee7
yan <yan.li@hp2.com>
2026-01-20 08:07:46 +0100

    add output for memory leak test 8

--- java.troubleshooting.oom/MemoryLeakTest8.java
+++ java.troubleshooting.oom/MemoryLeakTest8.java
@@ -9,3 +9,11 @@ public class MemoryLeakTest8 {
         ByteBuffer.allocateDirect(1024 * 1024 * 11);
     }
 }
+/**
+   Output:
+     Exception in thread "main" java.lang.OutOfMemoryError: Cannot reserve 11534336 bytes of direct buffer memory (allocated: 0, limit: 10485760)
+        at java.base/java.nio.Bits.reserveMemory(Bits.java:178)
+        at java.base/java.nio.DirectByteBuffer.<init>(DirectByteBuffer.java:111)
+        at java.base/java.nio.ByteBuffer.allocateDirect(ByteBuffer.java:360)
+        at MemoryLeakTest8.main(MemoryLeakTest8.java:9)
+ */
