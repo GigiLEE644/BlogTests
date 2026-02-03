a777ad2256d71bf1b854ac6558c857bd1bc3ecf4
ovo <ghub8551@gmail.com>
2026-02-03 08:34:10 +0100

    add output for java 8 new features test 43 and 44

--- java.features/8/Java8NewFeaturesTest43.java
+++ java.features/8/Java8NewFeaturesTest43.java
@@ -25,3 +25,29 @@ public class Java8NewFeaturesTest43 {
         });
     }
 }
+/**
+ * Output:
+        v1 = 4
+        v2 = 4
+        
+        key = 1
+        value = 2
+        key = 2
+        value = 4
+        key = 3
+        value = 6
+        key = 4
+        value = 8
+        
+        Exception in thread "main" java.lang.IllegalStateException: Duplicate key 2 (attempted merging values 4 and 4)
+                at java.base/java.util.stream.Collectors.duplicateKeyException(Collectors.java:135)
+                at java.base/java.util.stream.Collectors.lambda$uniqKeysMapAccumulator$0(Collectors.java:182)
+                at java.base/java.util.stream.ReduceOps$3ReducingSink.accept(ReduceOps.java:169)
+                at java.base/java.util.Spliterators$ArraySpliterator.forEachRemaining(Spliterators.java:1024)
+                at java.base/java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:570)
+                at java.base/java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:560)
+                at java.base/java.util.stream.ReduceOps$ReduceOp.evaluateSequential(ReduceOps.java:921)
+                at java.base/java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:265)
+                at java.base/java.util.stream.ReferencePipeline.collect(ReferencePipeline.java:723)
+                at Java8NewFeaturesTest43.main(Java8NewFeaturesTest43.java:18)
+ */
