77f2a9c3715654421ecca7528f7c241182f24647
ovo <ghub8551@gmail.com>
2026-03-11 11:57:18 +0100

    use thread factory of thread of virtual

--- java.features/21/Java21NewFeaturesTest10.java
+++ java.features/21/Java21NewFeaturesTest10.java
@@ -6,13 +6,7 @@ import java.util.UUID;
 public class Java21NewFeaturesTest10 {
 
         public static void main(String[] args) throws InterruptedException {
-                ThreadFactory base = Thread.ofVirtual().factory();
-
-                ThreadFactory f = r -> {
-                        Thread t = base.newThread(r);
-                        t.setName("app-" + UUID.randomUUID());
-                        return t;
-                };
+                ThreadFactory f = Thread.ofVirtual().name("app-", 1).factory();
 
                 ExecutorService e = Executors.newFixedThreadPool(2, f);
 
@@ -29,14 +23,14 @@ public class Java21NewFeaturesTest10 {
         }
 }
 /* Output:
-        Running task in thread : VirtualThread[#26,app-4efb38de-2044-4e60-9509-d3ca346b1975]/runnable@ForkJoinPool-1-worker-2
-        Running task in thread : VirtualThread[#26,app-4efb38de-2044-4e60-9509-d3ca346b1975]/runnable@ForkJoinPool-1-worker-2
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-1
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#24,app-264d5360-13bd-4566-b4c0-942db27dc027]/runnable@ForkJoinPool-1-worker-3
-        Running task in thread : VirtualThread[#26,app-4efb38de-2044-4e60-9509-d3ca346b1975]/runnable@ForkJoinPool-1-worker-2
+        Running task in thread : VirtualThread[#26,app-2]/runnable@ForkJoinPool-1-worker-2
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#26,app-2]/runnable@ForkJoinPool-1-worker-2
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#26,app-2]/runnable@ForkJoinPool-1-worker-2
+        Running task in thread : VirtualThread[#24,app-1]/runnable@ForkJoinPool-1-worker-1
+        Running task in thread : VirtualThread[#26,app-2]/runnable@ForkJoinPool-1-worker-2
  */
