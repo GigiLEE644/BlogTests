5a73f84a185bf15f3b1afaec9d355d5affa72103
ovo <ghub8551@gmail.com>
2026-03-17 15:29:27 +0100

    update spel example 1

--- java.spring/spel/src/main/java/com/example/spel/SpELExample1.java
+++ java.spring/spel/src/main/java/com/example/spel/SpELExample1.java
@@ -3,21 +3,24 @@ package com.example.spel;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class SpELExample1 {
 
     public static void main(String[] args) {
-
         ExpressionParser parser = new SpelExpressionParser();
 
-        Expression exp = parser.parseExpression("'Hello World from SpEL'");
+        StandardEvaluationContext context = new StandardEvaluationContext();
+        context.setVariable("greeting", "Hello World from SpEL");
 
-        String value = exp.getValue(String.class);
+        Expression exp = parser.parseExpression("#greeting");
+
+        String value = exp.getValue(context, String.class);
 
         System.out.println(value);
     }
 }
 /**
- * Output:
- * Hello World from SpEL
- */
+    Output:
+        Hello World from SpEL
+*/
