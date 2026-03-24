7ade7189bce07040175503877ee63e39d6bbeed6
ovo <ghub8551@gmail.com>
2026-03-24 16:11:25 +0100

    add spel example 20

---
+++ java.spring/spel/src/main/java/com/example/spel/SpELExample20.java
@@ -0,0 +1,23 @@
+package com.example.spel;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class SpELExample20 {
+    public static void main(String[] args) throws NoSuchMethodException {
+        record Person(String name, int age) {
+        }
+
+        Person tom = new Person("tom", 18);
+
+        ExpressionParser parser = new SpelExpressionParser();
+
+        String result = parser.parseExpression(
+                "age >= 18 ? 'Adult' : 'Minor'").getValue(tom, String.class);
+
+        System.out.println(result);
+    }
+}
+/* Output:
+ * Adult
+ */
