579fa0f2227b84a7b655f00f08f82197d73b8f34
ovo <ghub8551@gmail.com>
2026-03-24 16:23:34 +0100

    add spel example 21

---
+++ java.spring/spel/src/main/java/com/example/spel/SpELExample21.java
@@ -0,0 +1,40 @@
+package com.example.spel;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class SpELExample21 {
+    public static void main(String[] args) throws NoSuchMethodException {
+        record Person(String name, int age) {
+        }
+
+        ExpressionParser parser = new SpelExpressionParser();
+
+        String result = parser.parseExpression("name ?: 'Anonymous'").getValue(new Person(null, 18), String.class);
+
+        System.out.println(result);
+
+        System.out.println();
+
+        System.out.println(parser.parseExpression("name ?: 'Anonymous'").getValue(new Person("tom", 18), String.class));
+
+        System.out.println();
+
+        System.out.println(parser.parseExpression("name != null ? name : 'Anonymous'").getValue(new Person(null, 18),
+                String.class));
+
+        System.out.println();
+
+        System.out.println(parser.parseExpression("name != null ? name : 'Anonymous'").getValue(new Person("tom", 18),
+                String.class));
+    }
+}
+/* Output:
+ * Anonymous
+ *
+ * tom
+ *
+ * Anonymous
+ *
+ * tom
+ */
