e5a1ed2198f23bd1a9506fc798ec3b384ac9deec
ovo <ghub8551@gmail.com>
2026-04-20 10:58:11 +0200

    add Testcontainers

---
+++ spring-testing-project/src/test/java/com/example/demo/repository/UserRepositoryTest2.java
@@ -0,0 +1,46 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@Testcontainers
+class UserRepositoryTest2 {
+    @Autowired
+    UserRepository userRepository;
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void shouldSaveAndFindUser() {
+        User user = new User();
+        user.setName("john");
+        user.setEmail("john@mail.com");
+
+        User saved = userRepository.save(user);
+
+        Optional<User> found = userRepository.findById(saved.getId());
+
+        assertThat(found).isPresent();
+    }
+}
